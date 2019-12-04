a59b102fa6241a1c79161c9a9adc81b8f2d9a854
hlaingtinhtun <hlaingtinhtun@gigas-jp.com>
2019-12-04 19:36:24 +0900

    building custom category adapter

--- app/src/main/java/com/example/swagshop/Controller/MainActivity.kt
+++ app/src/main/java/com/example/swagshop/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.swagshop.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.swagshop.Adapters.CategoryAdapter
 import com.example.swagshop.Model.Category
 import com.example.swagshop.R
 import com.example.swagshop.Services.DataService
@@ -10,15 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
